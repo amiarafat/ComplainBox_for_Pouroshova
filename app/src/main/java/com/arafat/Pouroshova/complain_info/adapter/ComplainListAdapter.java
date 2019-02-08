0f0c7529170187e7490eea36ebdd357a14ed8723
Arafat <arafat.ict.mbstu@gmail.com>
2019-02-08 20:14:04 +0600

    issue solved in list adapter

--- app/src/main/java/com/arafat/Pouroshova/complain_info/adapter/ComplainListAdapter.java
+++ app/src/main/java/com/arafat/Pouroshova/complain_info/adapter/ComplainListAdapter.java
@@ -1,8 +1,6 @@
 package com.arafat.Pouroshova.complain_info.adapter;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -40,9 +38,9 @@ public class ComplainListAdapter extends ArrayAdapter {
 
     }
 
-    @NonNull
+
     @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+    public View getView(int position, View convertView, ViewGroup parent) {
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_each_row_m_log, parent, false);
         }
